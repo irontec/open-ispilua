83587e04bfc5705c0136e1f71046234808c54642
Aitor Llamas Jimenez <aitorllj93@gmail.com>
2015-10-01 12:42:53 +0200

    refactor index code

---
+++ server/application.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var http = require('http');
+var socketio = require('socket.io');
+var express = require('express');
+var promises = require('bluebird');
+var request = promises.promisifyAll(require('request'));
+
+var APIManager = require('./helpers/api');
+var schedule = require('./schedule');
+
+function Application(config) {
+  this.config = config;
+  this.initialize();
+}
+
+Application.GEO_LOCATION_URL = 'http://ip-api.com/json';
+
+Application.prototype = {
+  initialize: function() {
+    this.app = this._promisify(express());
+    this.server = this._promisify(http.Server(this.app));
+    this.io = socketio(this.server);
+
+    if (!this.config.opendata || !this.config.opendata.url) {
+      throw 'Missing opendata config';
+    }
+
+    this.apiManager = new APIManager(this.config.opendata.url);
+
+  },
+  start: function() {
+    this.app.locals.api = this.apiManager;
+    this.app.locals.config = this.config;
+    this.app.locals.clients = [];
+    this.app.locals.io = this.io;
+
+    this._getGeolocation()
+      .then(this.serve());
+  },
+  serve: function() {
+
+    this.app.use('/static', express.static('../client'));
+
+    this.io.on('connection', function (socket) {
+
+        console.log('Client Connected');
+        this.app.locals.clients.push(socket);
+
+    }.bind(this));
+
+    return this.server.listenAsync(this.config.service.port)
+      .then(function() {
+
+        console.log('Server started at ', this.config.service.port);
+        schedule.start(this.app);
+
+      }.bind(this))
+      .catch(function(err) {
+
+        console.log('ERROR starting server');
+        console.log(err);
+
+      });
+  },
+  _getGeolocation: function() {
+    return request.getAsync(Application.GEO_LOCATION_URL)
+      .then(function(response) {
+        this.config.geo = JSON.parse(response[0].body);
+        return this.config.geo;
+      }.bind(this));
+  },
+  _promisify: function(obj) {
+    return promises.promisifyAll(obj);
+  }
+};
+
+
+module.exports = Application;
--- server/index.js
+++ server/index.js
@@ -1,73 +1,9 @@
 'use strict';
 
-var promisesManager = require('bluebird');
-
-var APIManager = require('./helpers/api');
-var request = require('request');
-promisesManager.promisifyAll(request);
+var Application = require('./application');
 
 var config = require('./config');
 
-var express = require('express');
-var app = express();
-promisesManager.promisifyAll(app);
+var application = new Application(config);
 
-
-var config = require('./config');
-
-var apiManager = new APIManager(config.opendata.url);
-
-
-var server = require('http').Server(app);
-promisesManager.promisifyAll(server);
-var io = require('socket.io')(server);
-
-getGeo()
-.then(function() {
-    startServer();
-})
-.catch(function(e) {
-    console.log('Error', e);
-    throw e;
-});
-
-function getGeo() {
-    return request.getAsync('http://ip-api.com/json')
-    .then(function(response) {
-        config.geo = JSON.parse(response[0].body);
-        return config.geo;
-    });
-}
-
-function startServer() {
-    app.locals.api = apiManager;
-    app.locals.config = config;
-    app.locals.clients = [];
-    app.locals.io = io;
-
-
-    // Serve Client app
-    app.use('/static', express.static('../client'));
-
-    io.on('connection', function (socket) {
-        console.log('Connected');
-        app.locals.clients.push(socket);
-    });
-
-
-    server.listenAsync(config.service.port)
-    .then(function() {
-        console.log('Server started at ', config.service.port);
-
-        require('./schedule')(app);
-    })
-    .catch(function(err) {
-        console.log('ERROR');
-        console.log(err);
-    });
-
-}
-
-
-
-
+application.start();
